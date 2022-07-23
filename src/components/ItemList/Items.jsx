602fc3edbb2c21118abe4619842b4f8d74034b41
AslanSN <aslan.staub@manumartinez.es>
2022-07-23 13:09:09 +0200

    Bug solved, back init

--- src/components/ItemList/Items.jsx
+++ src/components/ItemList/Items.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 /** Reducers */
 import {
-  toggleSelectedOf,
-  individualDeletion,
-  selectItem,
-} from "../../features/reducers/itemSlice";
+	toggleSelectedOf,
+	individualDeletion,
+	selectItem,
+} from '../../features/reducers/itemSlice'
 /** Styles */
-import { ItemStyled } from "./ItemStyled.js";
+import { ItemStyled } from './ItemStyled.js'
 /**
  * ! Component
  * * AslanSN - 22-07-02
  * @returns COMPONENT - React
  */
 const Items = () => {
-  const dispatch = useDispatch();
-  const list = useSelector(selectItem);
-  const selected = (id) => {
-    dispatch(toggleSelectedOf(id));
-  };
-  const deletion = (id) => {
-    dispatch(individualDeletion(id));
-  };
+	const dispatch = useDispatch()
+	const list = useSelector(selectItem)
+	const selected = (id) => {
+		dispatch(toggleSelectedOf(id))
+	}
+	const deletion = (id) => {
+		dispatch(individualDeletion(id))
+	}
 
-  return (
-    <ul>
-      {list.map((item) => (
-        <ItemStyled
-          className={item.isSelected ? "selected" : null}
-          key={item.id}
-          onClick={() => selected(item.id)}
-          onDoubleClick={() => deletion(item.id)}>
-          {item.content}{console.log(item)}
-        </ItemStyled>
-      ))}
-    </ul>
-  );
-};
+	return (
+		<ul>
+			{list.map((item) => (
+				<ItemStyled
+					className={item.isSelected ? 'selected' : null}
+					key={item.id}
+					onClick={() => selected(item.id)}
+					onDoubleClick={() => deletion(item.id)}>
+					{item.content}
+				</ItemStyled>
+			))}
+		</ul>
+	)
+}
 
-export default Items;
+export default Items
